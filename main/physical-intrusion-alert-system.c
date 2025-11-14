f5b7cd34a87347c9c59dea5b97b69548c233c7f5
fredd-deez <soksophal2004@gmail.com>
2025-11-14 14:25:40 +0700

    Updated main/

--- main/physical-intrusion-alert-system.c
+++ main/physical-intrusion-alert-system.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+
+#include "nvs_flash.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "lwip/err.h"
+#include "lwip/sys.h"
 
 void app_main(void)
 {
